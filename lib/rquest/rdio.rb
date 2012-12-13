78eec0c538a0d651387571598bff2d2d4f161ac9
Etienne Lemay <etiennelemay21@gmail.com>
2012-12-12 21:48:56 -0500

    Add authenticated api calls to Rdio API wrapper

--- lib/rquest/rdio.rb
+++ lib/rquest/rdio.rb
@@ -5,8 +5,8 @@ module Rdio
 
   base_uri 'http://api.rdio.com/1/?'
 
-  def self.post(params)
-    headers = {'Authorization' => oauth_header(__method__, base_uri, params)}
+  def self.post(params, tokens)
+    headers = {'Authorization' => oauth_header(__method__, base_uri, params, tokens)}
     super '', headers: headers, body: params
   end
 
@@ -18,8 +18,8 @@ module Rdio
   }
 
   METHODS.each do |wrapper_method, api_method|
-    define_singleton_method(wrapper_method) do |params = {}|
-      result = post params.merge(method: api_method)
+    define_singleton_method(wrapper_method) do |params = {}, tokens = {}|
+      result = post params.merge(method: api_method), tokens
       raise Error, result['message'] if result['status'] == 'error'
       result['result']
     end
@@ -27,8 +27,9 @@ module Rdio
 
 private
 
-  def self.oauth_header(method, url, params)
-    SimpleOAuth::Header.new(method, url, params,
-      consumer_key: ENV['RDIO_KEY'], consumer_secret: ENV['RDIO_SECRET']).to_s
+  def self.oauth_header(method, url, params, user_tokens)
+    tokens = { consumer_key: ENV['RDIO_KEY'], consumer_secret: ENV['RDIO_SECRET'] }
+    tokens = tokens.merge(user_tokens)
+    SimpleOAuth::Header.new(method, url, params, tokens).to_s
   end
 end
